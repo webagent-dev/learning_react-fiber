6bc430334c41cbb072c24fc83d9009f737726484
webagent-dev <webagent24@gmail.com>
2023-07-04 12:48:24 +0100

    add drop animation

--- src/comps/three.jsx
+++ src/comps/three.jsx
@@ -1,17 +1,59 @@
-import React from 'react'
-import 
+/* eslint-disable react/no-unknown-property */
+import React, { useEffect, useRef  }from 'react'
+import { angleToRadians } from '../utils/angle'
+import gsap from 'gsap'
+import { Environment, OrbitControls, PerspectiveCamera } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
 const Three = () => {
+  const orbitRef = useRef(null);
+  const ballRef = useRef(null);
+  useFrame((state) => {
+     if(!!orbitRef.current) {
+         const {x, y}  = state.mouse
+         orbitRef.current.setAzimuthalAngle(-x * angleToRadians(45));
+         orbitRef.current.setPolarAngle((y + 0.5) * angleToRadians(90-30));
+         orbitRef.current.update();
+      }
+  })
+
+  useEffect(() => {
+    if(!!ballRef.current){
+      const timeline = gsap.timeline({pause: true});
+      timeline.to(ballRef.current.position,{
+        x: 2,
+        duration: 2,
+        ease: "power2.out"
+      })
+      timeline.to(ballRef.current.position,{
+        y: 0.5,
+        duration: 2.0,
+        ease: "bounce.out"
+      }, "<")
+      timeline.play(); 
+    }
+  },[ballRef.current])
+  
   return (
     <>
-        <mesh>
-            <sphereGeometry args={[1, 32,32]}/>
+    <PerspectiveCamera makeDefault position={[0, 1, 5]}/>
+    <OrbitControls ref={orbitRef} minPolarAngle={angleToRadians(60)} maxPolarAngle={angleToRadians(80)}/>
+        <mesh position={[-2,2.5,0]} castShadow ref={ballRef}>
+            <sphereGeometry args={[0.6, 32,32]} metalness={0.6} roughness={0.2}/>
             <meshStandardMaterial  color='white'/>
         </mesh>
-        <mesh rotate={}>
-                  <planeGeometry args={[7,7]}/>
-                   <meshStandardMaterial  color='blue'/>
+        <mesh rotation={[ -(angleToRadians(90)), 0, 0]} receiveShadow>
+                  <planeGeometry args={[20,20]}/>
+                   <meshStandardMaterial  color='#0c30a5'/>
         </mesh>
-        <ambientLight color='white'/>
+        <ambientLight args={['white', 0.25]}/> 
+        <spotLight args={['white', 1.9, 10, angleToRadians(65), 0.8]} position={[-1,1,0.9]} castShadow/>
+        <Environment background>
+          <mesh>
+          <sphereGeometry args={[50, 100, 100]}/>
+          <meshBasicMaterial side={THREE.BackSide} color='#0d1fc1'  metalness={0.6} roughness={0.2}/>
+          </mesh>
+        </Environment>
     </>
   )
 }
--- src/utils/angle.js
+++ src/utils/angle.js
@@ -1,1 +1,1 @@
-export const angleToRadians = (angleInDeg) => (Math.PI / 180)
+export const angleToRadians = (angleInDeg) => (Math.PI / 180) * angleInDeg
